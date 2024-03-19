d6fec71bfa742059b8d76b1005f7f2bc90aac585
Ashlo <ashutoshbele5@gmail.com>
2024-03-20 00:01:50 +0530

    change

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import langchain
+import os
+from langchain import hub
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.llms import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+import chainlit as cl
+from langchain.chains import RetrievalQA
+
